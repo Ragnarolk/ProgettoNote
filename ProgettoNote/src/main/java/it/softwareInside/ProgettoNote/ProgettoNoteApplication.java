4ad0a3bea22668e145b57ed5785dd51f239fd6ba
Ragnarolk <126862333+Ragnarolk@users.noreply.github.com>
2023-03-21 15:36:51 +0100

    Update ProgettoNoteApplication.java

--- ProgettoNote/src/main/java/it/softwareInside/ProgettoNote/ProgettoNoteApplication.java
+++ ProgettoNote/src/main/java/it/softwareInside/ProgettoNote/ProgettoNoteApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProgettoNoteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProgettoNoteApplication.class, args);
+		SpringApplication.run(ProgettoNoteApplication.class, args)
 	}
 
 }
